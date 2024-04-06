a68066debbd7fa8354a673a8094377d0de50720e
Lucas Souza <shok@MacBook-e-Silva.local>
2024-04-06 11:27:52 -0300

    Inicializando o projeto

---
+++ whatTheCup/whatTheCup/whatTheCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatTheCupApp.swift
+//  whatTheCup
+//
+//  Created by Lucas Souza on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct whatTheCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
