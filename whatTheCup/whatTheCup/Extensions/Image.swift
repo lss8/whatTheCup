f4e07258353261803fa94726d3824c6d2b12c62c
Lucas Souza <shok@MacBook-e-Silva.local>
2024-04-06 14:46:18 -0300

    homeScreen midifi

---
+++ whatTheCup/whatTheCup/Extensions/Image.swift
@@ -0,0 +1,18 @@
+//
+//  Image.swift
+//  whatTheCup
+//
+//  Created by Lucas Souza on 06/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    static let icon = Icons()
+}
+
+struct Icons {
+    let logo = Image("logo")
+    let startButton = Image("startButton")
+}
